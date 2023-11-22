05e18fd788906a753533b3eb73a8ced68251e17b
Hraby <75084936+Hraby@users.noreply.github.com>
2023-11-22 20:26:23 +0100

    Hotfix

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,3 +1,4 @@
-export async function GET(request: Request) {
-    return new Response(JSON.stringify({ secret: "The cake is a lie" }))
+import { NextResponse } from "next/server"
+export async function GET() {
+    return NextResponse.json({secret: "The cake is a lie"})
 }

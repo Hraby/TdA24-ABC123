82d25ebdb61efcbf21afbc3e76870751377630e7
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2023-12-10 20:26:12 +0100

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client"
+import Homepage from "@/pages/homepage/homepage";
 
 const prisma = new PrismaClient()
 
 export default function Home() {
   return (
       <div>
-        <span>Hello TdA</span>
+        <Homepage></Homepage>
       </div>
   )
 }
---
+++ src/pages/homepage/homepage.tsx
@@ -0,0 +1,16 @@
+import './_homepage.css';
+
+export default function Homepage() {
+    return (
+        <div>
+            <nav>
+                <img src="TeacherDigitalAgency_LOGO_colour-white.png" alt="Logo TDA"></img>
+                <ul>
+                    <li><a href="#nasi-lektori">NAŠI LEKTOŘI</a></li>
+                    <li><a href="#o-nas">O NÁS</a></li>
+                    <li><a href="#kontakt">KONTAKT</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}

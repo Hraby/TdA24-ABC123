82bd74d7222cad87afbdd77183c3b6c6205b1a0c
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2023-12-11 19:01:41 +0100

    responsibility update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client"
+import {PrismaClient} from "@prisma/client"
 import Homepage from "@/pages/homepage/homepage";
 
 const prisma = new PrismaClient()
 
 export default function Home() {
-  return (
-      <div>
-        <Homepage></Homepage>
-      </div>
-  )
+    return (
+        <div>
+            <Homepage></Homepage>
+        </div>
+    )
 }

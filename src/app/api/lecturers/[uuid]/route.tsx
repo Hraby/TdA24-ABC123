3d6faa25daa333a6526f6234f20f990bbc6c0660
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2023-12-30 00:12:04 +0100

    homepage finish

--- src/app/api/lecturers/[uuid]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { getLecturer } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: Request, { params }: { params: { uuid: string } }) {
-    const lecturer = await getLecturer(params.uuid)
-    if(lecturer == null)
-        return NextResponse.json({message: "User not found"}, {status: 404} )
-    return NextResponse.json(lecturer)
-}

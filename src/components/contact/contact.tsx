b689ce515015d41bf7d08550ab5d5a8bb39ad8e9
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-03-01 15:42:18 +0100

    contact change

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,9 +1,29 @@
-import "./contact.css"
+import "./contact.css";
+import React from "react";
 
-export default function Page(){
-    return(
-        <>
-            <a className="contact" href="mailto:info@tda.cz">info@tda.cz</a>
-        </>
-    )
+export default function Page() {
+    return (
+        <div className="container">
+            <div className="left-column">
+                <div className="contact-info">
+                    <div className="contact-item">
+                        <img src="/call.png"/>
+                        <a href="tel:+420123456789">+420 123 456 789</a>
+                    </div>
+                    <div className="contact-item">
+                        <img src="/email.png"/>
+                        <a className="contact" href="mailto:info@tda.cz">info@tda.cz</a>
+                    </div>
+                    <div className="contact-item">
+                        <img src="/location.png"/>
+                        <a>Dukelská 99, Zlín</a>
+                    </div>
+                </div>
+                <p className="additional-text">Neváhejte nás kontaktovat jednou z výše uvedených možností a nebo nás přijďte rovnou navštívít!</p>
+            </div>
+            <div className="right-column">
+                <img src="/contact-example.jpeg" className="rounded-image" />
+            </div>
+        </div>
+    );
 }
